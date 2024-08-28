eef438408ce2f9ffebb4e1a2d3828dfbb68c49f1
konradRduch <91778942+konradRduch@users.noreply.github.com>
2024-08-28 16:02:42 +0200

    Environment config and initialization

---
+++ Backend/grocipes/src/main/java/com/grocipes_backend/grocipes/GrocipesApplication.java
@@ -0,0 +1,13 @@
+package com.grocipes_backend.grocipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocipesApplication.class, args);
+	}
+
+}
---
+++ Backend/grocipes/src/test/java/com/grocipes_backend/grocipes/GrocipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocipes_backend.grocipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
