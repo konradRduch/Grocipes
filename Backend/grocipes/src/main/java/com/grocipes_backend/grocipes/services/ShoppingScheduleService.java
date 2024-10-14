43794369e29f1d3f8cfefd65d48187b7860d0a11
konradRduch <91778942+konradRduch@users.noreply.github.com>
2024-10-14 08:19:25 +0200

    backend entity update and created repository and service

--- Backend/grocipes/src/main/java/com/grocipes_backend/grocipes/services/ShoppingScheduleService.java
+++ Backend/grocipes/src/main/java/com/grocipes_backend/grocipes/services/ShoppingScheduleService.java
@@ -1,4 +1,14 @@
 package com.grocipes_backend.grocipes.services;
 
+import com.grocipes_backend.grocipes.repositories.ShoppingScheduleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ShoppingScheduleService {
+
+    private final ShoppingScheduleRepository shoppingScheduleRepository;
+
+    public ShoppingScheduleService(ShoppingScheduleRepository shoppingScheduleRepository) {
+        this.shoppingScheduleRepository = shoppingScheduleRepository;
+    }
 }
--- Backend/grocipes/src/main/java/com/grocipes_backend/grocipes/services/UnitProductShoppingListService.java
+++ Backend/grocipes/src/main/java/com/grocipes_backend/grocipes/services/UnitProductShoppingListService.java
@@ -1,4 +1,15 @@
 package com.grocipes_backend.grocipes.services;
 
+import com.grocipes_backend.grocipes.models.UnitProductShoppingList;
+import com.grocipes_backend.grocipes.repositories.UnitProductShoppingListRepository;
+import com.grocipes_backend.grocipes.repositories.UnitRecipeProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UnitProductShoppingListService {
+   private final UnitProductShoppingListRepository unitProductShoppingListRepository;
+
+    public UnitProductShoppingListService(UnitProductShoppingListRepository unitProductShoppingListRepository) {
+        this.unitProductShoppingListRepository = unitProductShoppingListRepository;
+    }
 }
